174c2b720aec70b765615c86dac5c080e88048aa
Steve Trombley <steve.trombley@bos-mac-stromble.hsd1.vt.comcast.net>
2017-07-17 10:56:50 -0400

    second commit

--- StevesFuzzyAdaptation/StevesFuzzyAdaptation/StevesFuzzyAdaptation.h
+++ StevesFuzzyAdaptation/StevesFuzzyAdaptation/StevesFuzzyAdaptation.h
@@ -14,6 +14,6 @@ FOUNDATION_EXPORT double StevesFuzzyAdaptationVersionNumber;
 //! Project version string for StevesFuzzyAdaptation.
 FOUNDATION_EXPORT const unsigned char StevesFuzzyAdaptationVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <StevesFuzzyAdaptation/PublicHeader.h>
+ 
 
 
